e543c709da8ec7bf91031e3e3cefaf1d8c630f61
rania-ibrhem30 <raniamansour967@gmail.com>
2023-09-18 21:06:19 +0300

    home-page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Routes } from '@angular/router';
+import { HomeComponent } from './Components-shared/home/home.component';
+import { FooterComponent } from './Components-shared/footer/footer.component';
+import { NavbarComponent } from './Components-shared/navbar/navbar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component:HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'footer', component: FooterComponent },
+  { path: 'navbar', component: NavbarComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Components-shared/home/home.component';
+import { NavbarComponent } from './Components-shared/navbar/navbar.component';
+import { FooterComponent } from './Components-shared/footer/footer.component';
+import { RouterOutlet } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
+import { PrimeNgModule } from './prime-ng/prime-ng.module';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterOutlet,
+    AppRoutingModule,
+    PrimeNgModule,
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
